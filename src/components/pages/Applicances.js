02913a1fec71d70fdaf9cf984129a62aef2f0b85
Dahlia Graves <dahlialolagraves@gmail.com>
2022-07-23 10:55:05 -0700

    added in cards to the appliances page and added photos for them

--- src/components/pages/Applicances.js
+++ src/components/pages/Applicances.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import '../../styles/home.css';
+import '../../styles/appliances.css';
 
 export default function Appliances() {
-    return(
-        <div>
-            <p>Howdy Ho</p>
+    return (
+        <div className="parentDiv">
+        <div class="l-container">
+            <div class="b-game-card">
+                <div class="b-game-card__cover"><img className="cardImages" src="/TinyBathroom.jpg" alt="slider2" /><h1> Bathroom</h1></div>
+            </div>
+            <div class="b-game-card">
+                <div class="b-game-card__cover" ><img className="cardImages" src="/Kitchen.jpg" alt="slider2" /></div>
+            </div>
+            <div class="b-game-card">
+                <div class="b-game-card__cover" > <img className="cardImages" src="/TinyWasher.jpg" alt="slider2" /></div>
+            </div>
+            <div class="b-game-card">
+                <div class="b-game-card__cover" ><img className="cardImages" src="/MiniSplit.jpg" alt="slider2" /></div>
+            </div>
+        </div>
         </div>
     )
 }
